a1910d7255d5d62e199fb588625bc2945b1cc1a5
Felipe Jung <felipemattoseu@gmail.com>
2024-11-09 14:53:56 -0400

    Add react-helmet-async

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import { router } from "./routes";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | MBA Marketplace" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
